994e91264dc179d2403f1288adae84b0f4d24204
Nathakorn <korn.za@windowslive.com>
2016-05-07 18:21:22 +0700

    prepare login

--- src/js/controller/userProfile.js
+++ src/js/controller/userProfile.js
@@ -1,15 +1,31 @@
 angular.module('regisKuApp', ['ui.router'])
-  .controller('userProfileController', ['$scope', '$http', 
-    function ($scope, $http) {
+  .controller('userProfileController', ['$scope', '$http', '$stateParams',
+    function ($scope, $http, $stateParams) {
     //Mock up database
     //Insert
+    
+    /*
     $http.post('http://52.37.98.127:3000/v1/5610545668?pin=1029', {
           "5610545001": {
               "name": "",
               "courses": ["123458", "1234567"]
-              ""
           },
-          
+          "5610545002": {
+              "name": "",
+              "courses": ["123458", "1234567"]
+          },
+          "5610545003": {
+              "name": "",
+              "courses": ["123458", "1234567"]
+          },
+          "5610545004": {
+              "name": "",
+              "courses": ["123458", "1234567"]
+          },
+          "5610545005": {
+              "name": "",
+              "courses": ["123458", "1234567"]
+          },
 
         })
         .success(function (data) {
@@ -24,12 +40,12 @@ angular.module('regisKuApp', ['ui.router'])
     //delete
     $http.delete('http://52.37.98.127:3000/v1/5610545668/5555555558?pin=1029')
     $http.delete('http://52.37.98.127:3000/v1/5610545668/5555555556?pin=1029')
-        
-
+    */   
+    //Get profile
     $scope.userProfile;
-    //get
-    $scope.getUserProfile = function () {
-      $http.get('http://52.37.98.127:3000/v1/5555555555/5555555556?pin=1234')
+    $scope.ID = $stateParams.ID;
+    console.log($stateParams.ID);
+    $http.get('http://52.37.98.127:3000/v1/5610545668/' + $scope.ID + '?pin=1029')
         .success(function (data){
           console.log(data);
           $scope.userProfile = data;
@@ -38,6 +54,9 @@ angular.module('regisKuApp', ['ui.router'])
           console.log(data);
           $state.go("404");
         });
+    //reserve
+    $scope.getUserProfile = function () {
+      
     };
 
 }]);
