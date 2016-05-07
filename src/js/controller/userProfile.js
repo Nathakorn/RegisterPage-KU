ae1aa2befa5cdc99d7d828f3d3bd13ee77a19a40
Nathakorn <korn.za@windowslive.com>
2016-05-07 23:26:49 +0700

    Fix hard bug

--- src/js/controller/userProfile.js
+++ src/js/controller/userProfile.js
@@ -1,5 +1,5 @@
-angular.module('regisKuApp', ['ui.router'])
-  .controller('userProfileController', ['$scope', '$http', '$stateParams',
+var app = angular.module('regisKuApp', ['ui.router']);
+  app.controller('userProfileController', ['$scope', '$http', '$stateParams',
     function ($scope, $http, $stateParams) {
     //Mock up database
     //Insert
@@ -58,5 +58,45 @@ angular.module('regisKuApp', ['ui.router'])
     $scope.getUserProfile = function () {
       
     };
-
 }]);
+//home
+app.controller('TodoListController', ['$scope', 
+    function ($scope) {
+    var todoList = this;
+    $scope.testScope = 'korn';
+
+    $scope.todos = [
+      {text: 'learn angular', done: true},
+      {text: 'build an angular app', done: false}]
+
+    $scope.addTodo = function () {
+      todoList.todos.push({text: todoList.todoText, done: false})
+      todoList.todoText = ''
+    }
+
+    $scope.remaining = function () {
+      var count = 0
+      angular.forEach(todoList.todos, function (todo) {
+        count += todo.done ? 0 : 1
+      })
+      return count
+    }
+
+    $scope.archive = function () {
+      var oldTodos = todoList.todos
+      todoList.todos = []
+      angular.forEach(oldTodos, function (todo) {
+        if (!todo.done) todoList.todos.push(todo)
+      })
+    }
+}]);
+
+//login
+app.controller('loginController', ['$scope', '$http', '$stateParams', '$location',
+    function ($scope, $http, $stateParams, $location) {
+      $scope.ID = "";
+      $scope.login = function () {
+        console.log($scope.ID);
+        $location.path("/"+ $scope.ID + "/home");      
+      }
+  }]);
