7e7fef298cb4cb8cf85f9eef2c5706dbcc2b6e36
Nathakorn <korn.za@windowslive.com>
2016-05-09 04:32:27 +0700

    finish button

--- src/js/controller/allController.js
+++ src/js/controller/allController.js
@@ -154,26 +154,17 @@ app.controller('loginController', ['$scope', '$http', '$stateParams', '$location
 //register
 app.controller('registerController', ['$scope', '$http', '$stateParams', '$location',
     function ($scope, $http, $stateParams, $location) {
+      $scope.selectedSubject = "";
       $scope.states = [];
       $http.get('http://52.37.98.127:3000/v1/5610545668/subjects?pin=1029')
           .success(function (data){
-            /* 
-              var fullname = "";
-              int i;
-              for (i = 0; i < 2; i++) { 
-                fullname += data[i];
-              }
-            */
-            angular.forEach(data, function(subject){
+              angular.forEach(data, function(subject){
               console.log(subject.subID + subject.subName);
               $scope.states.push(subject.subID +" - "+ subject.subName);  
             });
+      })
+
       
-          })
-          
-      
-      $scope.selectedState = "";
-      //$scope.states = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
       
       
       $scope.course;
